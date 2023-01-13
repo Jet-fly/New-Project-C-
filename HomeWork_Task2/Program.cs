b0ecf1e34c9229c0cf870194aae469393e39d3e3
Alexey <anikiforovki16@gmail.com>
2023-01-14 04:50:34 +0800

    homework_task2

---
+++ HomeWork_Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Type a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Type b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max;
+
+if (a > b)
+{
+    max = a;
+    Console.WriteLine("Max = " + a);
+}
+else
+{
+    max = b;
+    Console.WriteLine("Max = " + b);
+}
