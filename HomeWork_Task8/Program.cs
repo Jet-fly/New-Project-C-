a66504ee36772d5553c5893e7c5bb6389eaa4afc
Alexey <anikiforovki16@gmail.com>
2023-01-14 05:34:24 +0800

    task 8 done

--- HomeWork_Task8/Program.cs
+++ HomeWork_Task8/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Type N: ");
 int N = Convert.ToInt32(Console.ReadLine());
-
+ 
 for (int i = 1; i <= N; i++)
 {
     if (i % 2 == 0 && i != N && i != N-1)       // Сложные условия для красивого вывода.
