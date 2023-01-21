f77f70a79d5e7eaf386aebaf77228b95aee305fe
Alexey <anikiforovki16@gmail.com>
2023-01-21 10:55:28 +0800

    ended version

--- Seminar_task1/Program.cs
+++ Seminar_task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = new Random().Next(10,100);
+Console.WriteLine("a = " + a);
+
+int firstNumber = a / 10;
+int secondNumber = a % 10;
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine("max = " + firstNumber);
+}
+else
+{
+    Console.WriteLine("max = " + secondNumber);
+}
