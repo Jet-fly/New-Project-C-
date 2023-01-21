181a0af007a6446cf590b71855225abf8f2c4e4b
Alexey <anikiforovki16@gmail.com>
2023-01-21 14:14:48 +0800

    Task10 complete

--- HomeWork_Task10/Program.cs
+++ HomeWork_Task10/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a > 99 && a < 1000)
+{
+    int b = (a / 10) % 10;
+    System.Console.WriteLine("b = " + b);
+}
+else System.Console.WriteLine("Введено не трехзначное число");
