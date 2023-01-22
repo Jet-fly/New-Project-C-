c9cc822b835928552dde3151394cf2aad2d0144c
Alexey <anikiforovki16@gmail.com>
2023-01-22 09:42:36 +0800

    initial commit

---
+++ HomeWork_Task13/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+while (a/10 != 0)
+{
+    count++;
+}
+System.Console.WriteLine("count = " + count);
