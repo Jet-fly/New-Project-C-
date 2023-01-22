98b8ce7bcf025817e039c68a605c6c5b1001e51a
Alexey <anikiforovki16@gmail.com>
2023-01-22 10:48:16 +0800

    Task15 done

---
+++ HomeWork_Task15/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.Write("Введите цифру дня недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a <= 7)
+{
+    if (a == 6 || a == 7)
+    {
+        System.Console.WriteLine("Yes");
+    }
+    else System.Console.WriteLine("No");
+}
+else System.Console.WriteLine("Не является днем недели");
