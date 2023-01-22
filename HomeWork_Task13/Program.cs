98b16ad364351a0cece2a702d3ec86a7ef53d1eb
Alexey <anikiforovki16@gmail.com>
2023-01-22 10:37:19 +0800

    task13 done

--- HomeWork_Task13/Program.cs
+++ HomeWork_Task13/Program.cs
@@ -1,9 +1,24 @@
 ﻿System.Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-int count = 0;
+int b = a;
+int count = 1;
 
-while (a/10 != 0)
+while (b/10 != 0) // Проверка количества цифр
 {
+    b = b/10;
     count++;
 }
-System.Console.WriteLine("count = " + count);
+System.Console.WriteLine("Количество цифр: " + count); // Выводим количество цифр
+
+int[] array = new int[count]; // Создали массив, в который запишем цифры числа в обратном порядке
+for (int i = 0; i < count; i++)
+{
+    array[i] = a % 10;
+    a = a / 10;
+}
+
+if (count >= 3) // Если количество цифр больше или равно 3, тогда...
+{
+    System.Console.WriteLine("Третья цифра: " + array[count-3]);
+}
+else System.Console.WriteLine("Третьей цифры нет");
