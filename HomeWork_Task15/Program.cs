0dd4f7d25d215509daf7d8e5901fab85f66a666a
Alexey <anikiforovki16@gmail.com>
2023-01-22 13:23:12 +0800

    Task15 with 2 variants

--- HomeWork_Task15/Program.cs
+++ HomeWork_Task15/Program.cs
@@ -1,4 +1,4 @@
-﻿System.Console.Write("Введите цифру дня недели: ");
+﻿/* System.Console.Write("Введите цифру дня недели: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
 if (a <= 7)
@@ -9,4 +9,19 @@ if (a <= 7)
     }
     else System.Console.WriteLine("No");
 }
-else System.Console.WriteLine("Не является днем недели");
+else System.Console.WriteLine("Не является днем недели"); */
+
+System.Console.Write("Введите цифру дня недели: ");
+string? a = Console.ReadLine();
+
+switch (a)
+{
+    case "1": System.Console.WriteLine("No"); break;
+    case "2": System.Console.WriteLine("No"); break;
+    case "3": System.Console.WriteLine("No"); break;
+    case "4": System.Console.WriteLine("No"); break;
+    case "5": System.Console.WriteLine("No"); break;
+    case "6": System.Console.WriteLine("Yes"); break;
+    case "7": System.Console.WriteLine("Yes"); break;
+    default: System.Console.WriteLine("Не является днем недели"); break;
+}
