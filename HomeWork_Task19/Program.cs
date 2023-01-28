085a32a2e479bc759fe8e7792bf526ff9bfe549c
Alexey <anikiforovki16@gmail.com>
2023-01-29 03:20:32 +0800

    task 19 complete

--- HomeWork_Task19/Program.cs
+++ HomeWork_Task19/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a;
+int count = 0;
+
+while (b > 0)
+{
+    b = b / 10;
+    count++;
+}
+
+if (count == 0)
+{
+    count++;
+}
+
+System.Console.WriteLine("Количество цифр: " + count);
+b = a;
+
+int[] number = new int[count];
+for (int i = count-1; i >= 0; i--)
+{
+    number[i] = b % 10;
+    b = b / 10;
+}
+
+bool f = true;
+int k = 0;
+int j = count -1;
+while (k < j)
+{
+    if (number[k] != number[j])
+    {
+        f = false;
+        System.Console.WriteLine("NO");
+        break;
+    }
+    else
+    {
+        k++;
+        j--;
+    }
+}
+if(f)
+{
+    System.Console.WriteLine("YES");
+}
