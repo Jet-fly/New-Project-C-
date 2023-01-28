36c58978ef4f095d534c159ee9497ace604f08aa
Alexey <anikiforovki16@gmail.com>
2023-01-29 05:10:43 +0800

    task 23 complete

---
+++ HomeWork_Task23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double b = 0;
+
+for (double i = 1; i <= n; i++)
+{
+    b = Math.Pow(i, 3);
+    if ( i != n )
+    {
+        System.Console.Write(b + ", ");
+    }
+    else System.Console.Write(b + ".");
+}
