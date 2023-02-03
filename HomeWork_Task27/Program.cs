fa3d2b632515f21ca12aad06b13be90432b03014
Alexey <anikiforovki16@gmail.com>
2023-02-03 12:00:54 +0800

    Task 27 complete

---
+++ HomeWork_Task27/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+while (a != 0)
+{
+    b = b + (a % 10);
+    a = a / 10;
+}
+System.Console.WriteLine("Result = " + b);
