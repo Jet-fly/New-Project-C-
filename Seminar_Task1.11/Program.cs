83aec426491b6755ccec5916945f56798d9561cf
Alexey <anikiforovki16@gmail.com>
2023-02-03 12:04:24 +0800

    done

---
+++ Seminar_Task1.11/Program.cs
@@ -0,0 +1,16 @@
+﻿//Z1.11. Найти: все трехзначные числа, чьи квадраты оканчиваются тремя цифрами, которые и составляют искомые числа;
+
+int a;
+int b;
+
+for (int i = 100; i < 1000; i++)
+{
+    a = i * i;
+    b = a % 1000;
+    if (i == b)
+    {
+        System.Console.WriteLine($"Число: {i}");
+        System.Console.WriteLine($"Квадрат числа: {a}");
+        System.Console.WriteLine();
+    }
+}
