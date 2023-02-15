a2ab41754d6242f5dd0108fe80687bf92009122f
Alexey <anikiforovki16@gmail.com>
2023-02-15 12:10:44 +0800

    50, 52 done

---
+++ homework_task50/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет */
+
+System.Console.Write("Введите количество столбцов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество строк в массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Random rnd = new Random();
+int[,] mat = new int[m, n];
+
+for (int i = 0; i < mat.GetLength(0); i++)
+{
+    for (int j = 0; j < mat.GetLength(1); j++)
+    {
+        mat[i, j] = rnd.Next(0, 100);
+        System.Console.Write("  " + mat[i, j]);
+    }
+    System.Console.WriteLine();
+}
+
+System.Console.Write("Введите столбец: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите строку: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if ((a > 0 && a <= n) && (b > 0 && b <= m))
+{
+    System.Console.WriteLine("Ваш элемент: " + mat[b-1,a-1]);
+}
+else
+{
+    System.Console.WriteLine("Такого элемента нет");
+}
---
+++ homework_task52/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
+
+System.Console.Write("Введите кол-во столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите кол-во строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Random rnd = new Random();
+int[,] mat = new int[m, n];
+
+double sred = 0;
+
+for (int i = 0; i < mat.GetLength(0); i++)
+{
+    for (int j = 0; j < mat.GetLength(1); j++)
+    {
+        mat[i, j] = rnd.Next(0, 100);
+        System.Console.Write(mat[i, j] + "\t");
+
+    }
+    System.Console.WriteLine();
+}
+
+for (int i = 0; i < mat.GetLength(1); i++)
+{
+    for (int j = 0; j < mat.GetLength(0); j++)
+    {
+        sred = sred + mat[j,i];
+    }
+    sred = sred / mat.GetLength(0);
+    System.Console.WriteLine($"Столбец {i+1} = " + "{0:F1}", sred);
+    sred = 0;
+}
